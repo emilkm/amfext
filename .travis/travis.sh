b72f463f7fa1199af37ee54a306b03a8931e0bbc
emilkm <emilkm@users.noreply.github.com>
2017-02-03 20:04:17 +1300

    Add travis script

---
+++ .travis/travis.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+function build_extension() {
+    mkdir "$AMFEXT_BUILD_DIR"
+    
+    pushd "${AMFEXT_BUILD_DIR}"
+        phpize
+        ./configure --enable-debug --disable-all --enable-libxml --enable-simplexml --enable-dom --with-amf
+        make
+        make install
+    popd
+}
+
+function run_tests() {
+    export NO_INTERACTION=1
+    export REPORT_EXIT_STATUS=1
+    export TEST_PHP_EXECUTABLE=$(which php)
+
+    pushd "${AMFEXT_BUILD_DIR}"
+        php run-tests.php --show-diff -d extension=modules/amf.so -n ./tests/*.phpt
+        retval=$?
+    popd
+    return $retval;
+}
+
+# Command line arguments
+ACTION=$1
+
+# Where to do the build
+AMFEXT_BUILD_DIR="/tmp/amfext-build"
+
+set -e
+
+case $ACTION in
+    before_script)
+        # Nothing to do at the moment
+
+    ;;
+
+    script)
+        # Build the extension
+        build_extension
+
+        # Run tests
+        set +e
+        run_tests || exit 1
+    ;;
+
+    *)
+        echo "Unknown action. Valid actions are: before_script and script"
+        exit 1
+    ;;
+esac
+
+
+
+
+
