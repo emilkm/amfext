c7cbb920c0e1bd747de7728454d96ba83586b502
emilkm <emilkm@users.noreply.github.com>
2017-02-03 16:06:05 +1300

    Add license comment to the header file.

--- php_amf.h
+++ php_amf.h
@@ -1,3 +1,17 @@
+/**
+ * PHP7 extension for Action Message Format (AMF) encoding and decoding with support for AMF0 and AMF3
+ * 
+ * amfext (http://emilmalinov.com/amfext)
+ *
+ * @copyright Copyright (c) 2015 Emil Malinov
+ * @license   http://www.apache.org/licenses/LICENSE-2.0 Apache License 2.0
+ * @link      http://github.com/emilkm/amfext
+ * @package   amfext
+ * 
+ * @author    Emanuele Ruffaldi emanuele.ruffaldi@gmail.com - original version for PHP 5.2
+ * @author    Emil Malinov - PHP 7.X, PHP 5.X maintenance, unit tests, enhancements, bug fixes                                
+ */
+
 #ifndef PHP_AMF_H
 #define PHP_AMF_H 1
 
